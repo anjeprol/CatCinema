5b35d8638bf646d729a54680b82ce6586dbe6193
antoniopradoo <antoniopradoo@gmail.com>
2017-10-29 21:35:10 -0600

    - fixed bug with permissions

--- app/src/main/java/com/pramont/catcines/catcinema/MainActivity.java
+++ app/src/main/java/com/pramont/catcines/catcinema/MainActivity.java
@@ -2,8 +2,6 @@ package com.pramont.catcines.catcinema;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
